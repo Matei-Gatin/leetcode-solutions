ce5c63875d7957eff5e530e7ec5a114653b9cfef
Matei <thedackchair@gmail.com>
2026-09-19 14:15:11 +0000

    Sync LeetCode submission Runtime - 46 ms (66.60%), Memory - 29 MB (92.37%)

--- solutions/0015-3sum/solution.cpp
+++ solutions/0015-3sum/solution.cpp
@@ -3,27 +3,34 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // [-1,0,1,2,-1,-4]
-        // [-4,-1,-1,0,1,2,2]
+        // a + b + c = 0;
+        /*
+            [-1,0,1,2,-1,-4]
+            [-4,-1,-1,-1,0,0,2,3] O(NlogN)
 
-        // we sort the array to get rid of duplicate triplets
-        
+            sort the array O(NlogN)
+            use 3 pointers
+        */
+
         sort(nums.begin(), nums.end());
 
         vector<vector<int>> res;
 
         for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
                 continue;
             }
 
-            int l = i + 1;
-            int r = nums.size() - 1;
+            int l = i + 1, r = nums.size() - 1;
 
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
 
-                if (sum == 0) {
+                if (sum < 0) {
+                    l++;
+                } else if (sum > 0) {
+                    r--;
+                } else {
                     res.push_back({nums[i], nums[l], nums[r]});
 
                     l++;
@@ -32,10 +39,6 @@ public:
                     while (l < r && nums[l] == nums[l - 1]) {
                         l++;
                     }
-                } else if (sum < 0) {
-                    l++;
-                } else {
-                    r--;
                 }
             }
         }
