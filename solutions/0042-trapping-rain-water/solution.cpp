12ba86b26c87d2a616646a8bcf09e2779654288c
Matei <thedackchair@gmail.com>
2026-09-23 15:13:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 25.8 MB (99.73%)

--- solutions/0042-trapping-rain-water/solution.cpp
+++ solutions/0042-trapping-rain-water/solution.cpp
@@ -1,25 +1,23 @@
-using namespace std;
-
 class Solution {
 public:
     int trap(vector<int>& height) {
         int l = 0;
         int r = height.size() - 1;
-
-        int max_left = height[l];
-        int max_right = height[r];
+        
+        int max_l = height[l];
+        int max_r = height[r];
 
         int res = 0;
 
         while (l < r) {
-            if (max_left < max_right) {
+            if (max_l < max_r) {
                 l++;
-                max_left = max(max_left, height[l]);
-                res += max_left - height[l];
+                max_l = max(max_l, height[l]);
+                res += max_l - height[l];
             } else {
                 r--;
-                max_right = max(max_right, height[r]);
-                res += max_right - height[r];
+                max_r = max(max_r, height[r]);
+                res += max_r - height[r]; 
             }
         }
 
