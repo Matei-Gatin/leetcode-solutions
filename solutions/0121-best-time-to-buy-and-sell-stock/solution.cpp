285c973411ff9c0ecede8a8a1a9a9adebf773af3
Matei <thedackchair@gmail.com>
2026-09-23 13:59:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 97.4 MB (61.88%)

--- solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_buy_price = prices[0];
         int max_profit = 0;
+        int buy_price = prices[0];
 
         for (int i = 1; i < prices.size(); i++) {
-            int current_price = prices[i];
-
-            if (min_buy_price > current_price) {
-                min_buy_price = current_price;
-            } else {
-                int profit = current_price - min_buy_price;
-                max_profit = max(max_profit, profit);
+            if (prices[i] < buy_price) {
+                buy_price = prices[i];
             }
+
+            max_profit = max(max_profit, prices[i] - buy_price);
         }
 
         return max_profit;
